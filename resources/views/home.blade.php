0332e00820fe93d685312855e614ba89900577c0
Ciro Meluziis <meluziis.ciro99@gmail.com>
2023-01-09 18:01:14 +0100

    updated home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,10 @@
 @extends('layout.main')
 
 @section('content')
-    <h1>HOME</h1>
+    <section class="w-100 h-100 d-flex">
+        <div class="container m-auto flex-column d-flex justify-content-center align-items-center">
+            <h1>Questa è la Home Page</h1>
+            <h2 class="text-center">Per visualizzare la lista dei film cliccare su "Movies" nel menu di navigazione</h2>
+        </div>
+    </section>
 @endsection
